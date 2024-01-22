5a046ba95bb6dacfabd55bffee7f2a07c57270d5
vedanti <vedanti.r.bagade@gmail.com>
2024-01-22 22:41:27 +0530

    file updated

--- variables.swift
+++ variables.swift
@@ -4,9 +4,9 @@ print("Today is \(day). Rise and shine!")
 print("The temperature on \(day) is \(dailyTemperature)°F.")
 
 var temperature = 70
-print("The tempearature on \(day) morning is \(temperature)°F.")
+print("The temperature on \(day) morning is \(temperature)°F.")
 temperature = 80
-print("The tempearature on \(day) evening is \(temperature)°F.")
+print("The temperature on \(day) evening is \(temperature)°F.")
 
 let weeklyTemperature = 75
 temperature = weeklyTemperature
