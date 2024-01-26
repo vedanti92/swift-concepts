05346ee02a2ed78a03786c53f67a264a0d7d0ed9
vedanti <vedanti.r.bagade@gmail.com>
2024-01-26 23:38:11 +0530

    file updated

--- strings.swift
+++ strings.swift
@@ -4,4 +4,5 @@ print("Today is \(day).")
 let hour = "6"
 let minutes = "15"
 let period = "PM"
-var time = hour + " : " + minutes + " " + period
+var time = hour + ":" + minutes + " " + period
+print("It is \(time).")
