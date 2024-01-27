4a4587358c0ef63c94f5a3698b5135288418492d
vedanti <vedanti.r.bagade@gmail.com>
2024-01-27 19:17:42 +0530

    file updated

--- statements.swift
+++ statements.swift
@@ -1,2 +1,4 @@
 let freeApp = true
-if (freeApp == true)
+if (freeApp == true) {
+    print("You are using the free version of the app. Buy the full version of the app to get access to all of its features.")
+}
