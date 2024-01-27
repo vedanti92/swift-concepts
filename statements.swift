33bf4f900fc083e3a113e032ed84b9e2b760eb4f
vedanti <vedanti.r.bagade@gmail.com>
2024-01-27 19:19:25 +0530

    file updated

--- statements.swift
+++ statements.swift
@@ -2,3 +2,6 @@ let freeApp = true
 if (freeApp == true) {
     print("You are using the free version of the app. Buy the full version of the app to get access to all of its features.")
 }
+
+let morningTemperature = 70
+let eveningTemperature = 80
