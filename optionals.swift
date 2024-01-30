59f5650ca0716eb569715a3cfe7f2a97a61bfcd3
vedanti <vedanti.r.bagade@gmail.com>
2024-01-30 17:46:14 +0530

    file updated

--- optionals.swift
+++ optionals.swift
@@ -1,3 +1,9 @@
 var password = "1234"
 let passcode = Int(password)
 print("The passcode of the app is \(passcode!).")
+password = "hello world"
+if let code = Int(password) {
+    print("The passcode of the app is \(code).")
+} else {
+    print("Invalid passcode!")
+}
