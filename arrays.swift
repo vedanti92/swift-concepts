a5b9635d0c411b4723bebe3e1a7f90b8d3930236
vedanti <vedanti.r.bagade@gmail.com>
2024-01-31 18:33:16 +0530

    file updated

--- arrays.swift
+++ arrays.swift
@@ -10,3 +10,9 @@ levelScores[0] += levelBonusScore
 print("The first level's score is \(levelScores[0])!")
 let freeLevelScores = [20, 30]
 levelScores += freeLevelScores
+let freeLevels = 3
+if (levelScores.count == freeLevels) {
+    print("You have to buy the game in order to play its full version.")
+    levelScores = []
+    print("Game restarted!")
+}
