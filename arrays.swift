dd8d2e40229f72bab4bd5c6ff8b372181adb93cd
vedanti <vedanti.r.bagade@gmail.com>
2024-01-31 17:45:08 +0530

    file updated

--- arrays.swift
+++ arrays.swift
@@ -2,3 +2,6 @@ var levelScores: [Int] = []
 if (levelScores.count == 0) {
     print("Welcome!")
 }
+let firstLevelScore = 10
+levelScores.append(firstLevelScore)
+print("The first level's score is \(levelScores[0])!")
