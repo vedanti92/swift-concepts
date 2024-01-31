87679b12cb7e3bb46e123b20fcff7d996baea6b2
vedanti <vedanti.r.bagade@gmail.com>
2024-01-31 19:04:38 +0530

    file updated

--- tuples.swift
+++ tuples.swift
@@ -5,3 +5,8 @@ if (credentials.0 == "" || credentials.1 < 0) {
     print("The password is \(credentials.0) and the passcode is \(credentials.1).")
 }
 let fullCredentials = (password: "pass", passcode: 1111)
+if (fullCredentials.password == "" || fullCredentials.passcode < 0) {
+    print("Invalid credentials!")
+} else {
+    print("The password is \(fullCredentials.password) and the passcode is \(fullCredentials.passcode).")
+}
