371e752e4d08227c8b610ea8d8b66644a7b6bec1
vedanti <vedanti.r.bagade@gmail.com>
2024-01-31 19:01:02 +0530

    file updated

--- tuples.swift
+++ tuples.swift
@@ -2,5 +2,6 @@ let credentials = ("", -1111)
 if (credentials.0 == "" || credentials.1 < 0) {
     print("Invalid credentials!")
 } else {
-    print("The username is \(credentials.0) and the password is \(credentials.1).")
+    print("The password is \(credentials.0) and the passcode is \(credentials.1).")
 }
+let fullCredentials = (password: "pass", passcode: 1111)
