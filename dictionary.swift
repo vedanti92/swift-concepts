b680bcb56a834ad4424929eb3e4643146d99c21b
vedanti <vedanti.r.bagade@gmail.com>
2024-02-01 23:09:00 +0530

    file updated

--- dictionary.swift
+++ dictionary.swift
@@ -1,3 +1,4 @@
 var weeklyTemperatures: [String: Int] = [:]
 weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
 weeklyTemperatures["Monday"]! += 20
+print("The temperature on Monday is \(weeklyTemperatures["Monday"]!)°F.")
