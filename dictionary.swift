15698778f030da543e43650c127e28be84d4dd85
vedanti <vedanti.r.bagade@gmail.com>
2024-02-01 23:06:32 +0530

    file updated

--- dictionary.swift
+++ dictionary.swift
@@ -1,2 +1,3 @@
 var weeklyTemperatures: [String: Int] = [:]
 weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
+weeklyTemperatures["Monday"]! += 20
