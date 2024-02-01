e24a585680e88d6cf89497c56fc6917bc28b8eb9
vedanti <vedanti.r.bagade@gmail.com>
2024-02-01 23:17:59 +0530

    file updated

--- dictionary.swift
+++ dictionary.swift
@@ -2,3 +2,9 @@ var weeklyTemperatures: [String: Int] = [:]
 weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
 weeklyTemperatures["Monday"]! += 20
 print("The temperature on Monday is \(weeklyTemperatures["Monday"]!)°F.")
+if let temperature = weeklyTemperatures["Sunday"] {
+    print("The temperature on Sunday is \(temperature)°F.")
+} else {
+    weeklyTemperatures["Sunday"] = 100
+    print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F.")
+}
