b76130f4bd0fa7d940cbd581e3cd5a76339df7ed
vedanti <vedanti.r.bagade@gmail.com>
2024-02-02 22:51:50 +0530

    file updated

--- collection.swift
+++ collection.swift
@@ -12,3 +12,9 @@ let weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursda
 for (day, temperature) in weeklyTemperatures {
     print("The temperature on \(day) is \(temperature)°F.")
 }
+
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let temperatures = [70, 75, 80, 85, 90, 95, 100]
+for index in 0...6 {
+    print("The temperature on \(days[index]) is \(temperatures[index])°F.")
+}
