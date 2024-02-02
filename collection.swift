aee43987571dc3095e11c30ac6805a1e00bf7519
vedanti <vedanti.r.bagade@gmail.com>
2024-02-02 22:33:48 +0530

    file updated

--- collection.swift
+++ collection.swift
@@ -2,3 +2,8 @@ let levelScores: [Int] = [10, 20, 30, 40, 50, 60, 70]
 for (level, score) in levelScores.enumerated() {
     print("The score of level \(level+1) is \(score).")
 }
+var gameScore = 0
+for levelScore in levelScores {
+    gameScore += levelScore
+    print("The final game score is \(gameScore).")
+}
