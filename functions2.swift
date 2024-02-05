f2820758055af974ca1ee15ad9f7b8bfb1f61df3
vedanti <vedanti.r.bagade@gmail.com>
2024-02-05 18:02:20 +0530

    file updated

--- functions2.swift
+++ functions2.swift
@@ -1,4 +1,6 @@
 var goldBars = 0
-func incrementInventory(inventory: Int, amount: Int = 100) -> Int {
+func incrementInventory(inventory: Int, by amount: Int = 100) -> Int {
     return inventory + amount
 }
+goldBars = incrementInventory(inventory: goldBars, by: 200)
+print(goldBars)
