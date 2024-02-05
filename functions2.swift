4496fc3c8fe373552bdd2abcf41bf7a8bb8f14b1
vedanti <vedanti.r.bagade@gmail.com>
2024-02-05 17:56:46 +0530

    file updated

--- functions2.swift
+++ functions2.swift
@@ -1,4 +1,4 @@
 var goldBars = 0
-func incrementInventory(inventory: Int, amount: Int) -> Int {
+func incrementInventory(inventory: Int, amount: Int = 100) -> Int {
     return inventory + amount
 }
