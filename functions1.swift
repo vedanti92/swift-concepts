ba4f5d35a797ad10f994bad717e811bb5ea3ec77
vedanti <vedanti.r.bagade@gmail.com>
2024-02-05 17:47:23 +0530

    file updated

---
+++ functions1.swift
@@ -0,0 +1,10 @@
+var goldBars = 0
+func unlockTreasureChest(inventory: Int) -> Int {
+    return inventory + 100
+}
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
