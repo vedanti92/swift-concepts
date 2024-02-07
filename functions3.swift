61c450f18eb772fc8a2af1d7d8b1cd7eaa0e2309
vedanti <vedanti.r.bagade@gmail.com>
2024-02-07 20:38:34 +0530

    file updated

--- functions3.swift
+++ functions3.swift
@@ -6,4 +6,5 @@ func spendTenGoldBars(from inventory: inout Int, completion: (Int) -> Void) {
 print("You had \(goldBars) gold bars.")
 spendTenGoldBars(from: &goldBars) { goldBars in
     print("You spent ten gold bars.")
+    print("You have \(goldBars) gold bars.")
 }
