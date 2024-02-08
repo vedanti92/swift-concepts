c3568d71999e3100bed4f72acf500976db3a5699
vedanti <vedanti.r.bagade@gmail.com>
2024-02-08 13:21:57 +0530

    file updated

--- structures.swift
+++ structures.swift
@@ -1,6 +1,9 @@
 struct TableReservation {
     var name: String
     let tableNumber: Int
+    mutating func updateBooking(updatedName: String) {
+
+    }
 }
 var johnBooking = TableReservation(name: "John", tableNumber: 1)
 print(johnBooking)
