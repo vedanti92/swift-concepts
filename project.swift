de64e3b35c8659009de5236b7e03ede25a4b4a98
vedanti <vedanti.r.bagade@gmail.com>
2024-02-09 18:53:10 +0530

    file updated

--- project.swift
+++ project.swift
@@ -1,1 +1,13 @@
 // Project - Banking System
+
+class Greet {
+    let virtualBankSystem = ""
+    func welcome() {
+        print("Welcome to your virtual bank system.")
+    }
+    func accountType() {
+        print("What kind of account would you like to open?")
+        print("1. Debit account")
+        print("2. Credit account")
+    }
+}
